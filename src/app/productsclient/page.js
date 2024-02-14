9ecb34ce1d1e049c1ce75ff1e185f4c44ca9c93a
manish chauhan <manishkirthalya@gmail.com>
2024-02-14 13:22:25 +0530

    update

--- src/app/productsclient/page.js
+++ src/app/productsclient/page.js
@@ -1,6 +1,6 @@
 "use client"
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const page = () => {
     const [productData, setProductData] = useState([])
